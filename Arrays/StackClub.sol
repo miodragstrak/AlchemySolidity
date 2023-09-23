8b87e1ea32f76a5ed073d797b130721fd4e4f4f6
miodragstrak <44975850+miodragstrak@users.noreply.github.com>
2023-09-23 20:10:01 +0200

    Commit message

--- Arrays/StackClub.sol
+++ Arrays/StackClub.sol
@@ -2,32 +2,18 @@
 pragma solidity 0.8.4;
 
 contract StackClub {
+	address[] members; 
 
-    address[] members;
+	function isMember(address member) external view returns(bool) {
+		for(uint i = 0; i < members.length; i++) {
+			if(members[i] == member) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    constructor() {
-        members.push(msg.sender);
-    }
-
-    function addMember(address addr) external {
-        require(this.isMember(msg.sender), 'Throw');
-        members.push(addr);
-    }
-
-    function isMember(address x) external view returns(bool){
-        for (uint i = 0; i < members.length; i++) {
-            if(x == members[i]) {
-                return true;
-            }
-        }
-    
-    return false;
-        
-    }
-
-    function removeLastMember() external {
-        require(this.isMember(msg.sender), 'Throw');
-        members.pop();
-    }
-    
+	function addMember(address member) external {
+		members.push(member);
+	}
 }
