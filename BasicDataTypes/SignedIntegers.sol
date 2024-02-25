bb7db949f37d1f6bceabcd2ccc9b8a33708d2894
Miodrag Strak <44975850+miodragstrak@users.noreply.github.com>
2024-02-25 14:23:01 +0100

    MSG

---
+++ BasicDataTypes/SignedIntegers.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    int8 public a = 10;
+    int8 public b = -15;
+    int16 public difference = a - b;
+    
+    
+}
---
+++ BasicDataTypes/UnsignedIntegers.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint8 public a = 8;
+    uint public b = 257;
+    uint public sum = a + b;
+    
+}
