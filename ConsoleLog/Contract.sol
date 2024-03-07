7618597f43ba679241c193c643c8d917e72d9ee9
Miodrag Strak <44975850+miodragstrak@users.noreply.github.com>
2024-03-07 21:22:19 +0100

    Ono kao commit

---
+++ ConsoleLog/Contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/console.sol";
+
+contract Contract {
+    function winningNumber(string calldata secretMessage) external returns(uint) {
+        console.log(secretMessage);
+        return 794;        
+    }
+}
---
+++ PureDouble/Contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    function double(uint x) external pure returns(uint doublex) {
+        doublex = 2*x;
+
+    }
+
+}
