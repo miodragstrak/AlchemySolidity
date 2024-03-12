5d3c16f1c44b110dbe7e5d2edc56f27fa6f4dc56
Miodrag Strak <44975850+miodragstrak@users.noreply.github.com>
2024-03-12 12:39:29 +0100

    Commit

---
+++ OwnerModifier/Contract.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	uint configA;
+	uint configB;
+	uint configC;
+	address owner;
+
+	constructor() {
+		owner = msg.sender;
+	}
+
+	function setA(uint _configA) public onlyOwner {
+		configA = _configA;
+	}
+
+	function setB(uint _configB) public onlyOwner {
+		configB = _configB;
+	}
+
+	function setC(uint _configC) public onlyOwner {
+		configC = _configC;
+	}
+
+	modifier onlyOwner {
+		// TODO: require only the owner access
+		require(msg.sender == owner);
+		// TODO: run the rest of the function body
+		_;
+
+	}
+}
