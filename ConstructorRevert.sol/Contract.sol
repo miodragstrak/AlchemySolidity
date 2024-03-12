6cd86aa55414df52f7a4bfbe1cc664f3c06d8347
Miodrag Strak <44975850+miodragstrak@users.noreply.github.com>
2024-03-12 12:35:56 +0100

    Msg sender

--- ConstructorRevert.sol/Contract.sol
+++ ConstructorRevert.sol/Contract.sol
@@ -2,7 +2,15 @@
 pragma solidity ^0.8.20;
 
 contract Contract {
+	address owner;
+
 	constructor() payable {
+		owner = msg.sender;
 		require(msg.value >= 1 ether, "Not enough ether sent");
+	}
+
+	function withdraw() public {
+		require(msg.sender == owner);
+		payable(msg.sender).transfer(address(this).balance);
 	}
 }
