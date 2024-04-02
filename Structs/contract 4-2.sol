d6c0abaf338292fdd8be12dafe5dfc26a6ba4bde
Miodrag Strak <44975850+miodragstrak@users.noreply.github.com>
2024-04-02 21:47:24 +0200

    CMP

---
+++ Structs/contract 4-2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+	enum Choices { Yes, No }
+	
+	struct Vote {
+		Choices choice;
+		address voter;
+	}
+	
+	// TODO: create a public state variable: an array of votes
+
+	Vote[] public votes;
+
+	function createVote(Choices choice) external {
+		// TODO: add a new vote to the array of votes state variable
+		votes.push(Vote(choice, msg.sender));
+	}
+
+	function hasVoted(address _address) external view returns(bool) {
+
+	}
+
+	function findChoice(address _address) external view returns(Choices) {
+		
+	}
+}
---
+++ Structs/contract2-2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+	enum Choices { Yes, No }
+	
+	struct Vote {
+		Choices choice;
+		address voter;
+	}
+	
+	// TODO: create a public state variable: an array of votes
+
+	Vote[] public votes;
+
+	function createVote(Choices choice) external {
+		// TODO: add a new vote to the array of votes state variable
+		votes.push(Vote(choice, msg.sender));
+
+
+	}
+}
