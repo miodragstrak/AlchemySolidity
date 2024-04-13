684a00607460ab0961836b33c98bafeca1fc2a94
Miodrag Strak <44975850+miodragstrak@users.noreply.github.com>
2024-04-13 21:28:07 +0200

    Inheriatance

---
+++ Inheritance/Inherit.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "./Hero.sol";
+
+// TODO: create Mage/Warrior Heroes
+
+contract Mage is Hero(50) {
+
+}
+
+contract Warrior is Hero(200) {
+    
+}
